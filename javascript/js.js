fb51ee0001118fddb320e52aab74feaf8f29ebb2
Antonia Cerda <antoniacerdag@gmail.com>
2017-04-24 13:33:58 -0300

    actualización js

--- javascript/js.js
+++ javascript/js.js
@@ -1,6 +1,6 @@
-function convertirCelsius(far){
+function convertirCelsius(celsius){
   
-    var far = document.getElementById('fahrenheit').value;
+    var far = document.getElementById("celsius").value;
     	alert( Math.round((5/9) * (far - 32)));
 }
   
