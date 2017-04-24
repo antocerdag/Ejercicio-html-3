af8646e6128b643a7c5679bd84b1c43f2a8fed78
Antonia Cerda <antoniacerdag@gmail.com>
2017-04-24 12:54:05 -0300

    Actualización html y js

--- js.js
+++ js.js
@@ -1,1 +1,7 @@
-js.js
+function convertirCelsius(document.getElementById("fahrenheit").value){
+  var fahrenheit = arr.map(function(elem){
+    return Math.round((elem*9)/5+32);
+  });
+  return fahrenheit;
+}
+
