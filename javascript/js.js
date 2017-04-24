f38cd80060a20a9adaa8998b5ca6ba76fd072f9c
Antonia Cerda <antoniacerdag@gmail.com>
2017-04-24 13:17:32 -0300

    Actualización html y js

---
+++ javascript/js.js
@@ -0,0 +1,7 @@
+function convertirCelsius(far){
+  
+    var far = document.getElementById('fahrenheit').value;
+    	alert( Math.round((5/9) * (far - 32)));
+}
+  
+
--- js.js
+++
@@ -1,7 +0,0 @@
-function convertirCelsius(document.getElementById("fahrenheit").value){
-  var fahrenheit = arr.map(function(elem){
-    return Math.round((elem*9)/5+32);
-  });
-  return fahrenheit;
-}
-
